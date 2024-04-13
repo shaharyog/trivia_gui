b67a123b85cd0c89fa7dac794980e6157751a402
Shahar Yogev <shahar.yogev47@gmail.com>
2024-04-13 23:47:08 +0300

    wip

--- lib/login.dart
+++ lib/login.dart
@@ -1,128 +1,102 @@
 import 'package:flutter/material.dart';
-import 'signup.dart';
+import 'package:provider/provider.dart';
+import 'package:your_flutter_app_name/theme_provider.dart';
+import 'package:your_flutter_app_name/utils.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        automaticallyImplyLeading: false,
+        actions: [
+          Consumer<ThemeProvider>(
+            builder: (context, themeProvider, child) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: IconButton(
+                  icon: Icon(themeProvider.themeMode == ThemeMode.light
+                      ? Icons.brightness_2
+                      : Icons.brightness_6),
+                  onPressed: () {
+                    themeProvider.toggleTheme();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 400),
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "Login",
+                      style: Theme.of(context).textTheme.displayMedium,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("Login to your account",
+                        style: Theme.of(context).textTheme.headlineSmall),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 40),
+                  child: Column(
                     children: <Widget>[
-                      Text("Login", style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold
-                      ),),
-                      SizedBox(height: 20,),
-                      Text("Login to your account", style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.grey[700]
-                      ),),
+                      makeInput(label: "User Name"),
+                      makeInput(label: "Password", obscureText: true),
                     ],
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 40),
-                    child: Column(
-                      children: <Widget>[
-                        makeInput(label: "User Name"),
-                        makeInput(label: "Password", obscureText: true),
-                      ],
+                ),
+                FilledButton(
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                      EdgeInsets.symmetric(horizontal: 80, vertical: 20),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 40),
-                    child: Container(
-                      width: 200,
-                      height: 60,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        border: Border.all(color: Colors.black),
-                      ),
-                      child: MaterialButton(
-                        onPressed: () {},
-                        color: Color.fromRGBO(159, 182, 163, 1.0),
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
-                        child: Text(
-                          "Login",
-                          style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18,),
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text("Don't have an account? "),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/signup');
+                        },
+                        child: MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          child: Text(
+                            "Sign up",
+                            style:
+                                Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      decoration: TextDecoration.underline,
+                                    ),
+                          ),
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Don't have an account? "),
-                        Text("Sign up", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18)),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Visibility(
-              visible: MediaQuery.of(context).size.height > 900,
-              child: Container(
-                height: MediaQuery.of(context).size.height / 3,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/background.png'),
-                        fit: BoxFit.cover
-                    )
                 ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget makeInput({label, obscureText = false}) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          width: 600, // Set the width of the TextField
-          child: TextField(
-            obscureText: obscureText,
-            decoration: InputDecoration(
-              hintText: label,
-              hintStyle: TextStyle(color: Colors.grey.shade800),
-              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-              enabledBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey.shade400),
-              ),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.grey.shade400),
-              ),
-              filled: true, // Enable the background color
-              fillColor: Colors.grey.shade200, // Set the background color
+              ],
             ),
           ),
         ),
-        SizedBox(height: 30),
-      ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,52 @@
+
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:your_flutter_app_name/theme_provider.dart';
 import 'login.dart';
 import 'signup.dart';
+import 'package:provider/provider.dart';
+
+const _defaultLightColorScheme = ColorScheme.light();
+
+const _defaultDarkColorScheme = ColorScheme.dark();
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return DynamicColorBuilder(
+            builder: (lightColorScheme, darkColorScheme) =>
+                MaterialApp(
+                  theme: ThemeData(
+                    useMaterial3: true,
+                    colorScheme: lightColorScheme ?? _defaultLightColorScheme,
+                  ),
+                  darkTheme: ThemeData(
+                    useMaterial3: true,
+                    colorScheme: darkColorScheme ?? _defaultDarkColorScheme,
+                  ),
+                  themeMode: themeProvider.themeMode,
+                  debugShowCheckedModeBanner: false,
+                  title: 'Trivia - Shahar & Yuval',
+                  initialRoute: '/login',
+                  routes: {
+                    // When navigating to the "/" route, build the HomePage widget.
+                    '/login': (context) => LoginPage(),
+                    '/signup': (context) => SignupPage(),
+                  },
+                )
+        );
+      },
+    );
+  }
 }
